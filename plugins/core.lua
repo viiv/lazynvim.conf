cf571564df3971180cde9b5185d0a4bc8bf42c7f
王俊山 <viivor@gmail.com>
2024-04-25 22:15:26 +0800

    添加更多的配置

--- plugins/core.lua
+++ plugins/core.lua
@@ -1,3 +1,7 @@
+vim.g.loaded_ruby_provider = false
+vim.g.loaded_perl_provider = false
+vim.g.loaded_python_provider = false
+
 return {
   { "tanvirtin/monokai.nvim" },
 
@@ -6,5 +10,25 @@ return {
     opts = {
       colorscheme = "monokai_soda",
     },
+  },
+
+  { "yianwillis/vimcdoc" },
+  { "arkav/lualine-lsp-progress" },
+  { "folke/lsp-colors.nvim" },
+  { "stevearc/overseer.nvim" },
+
+  {
+    "uga-rosa/translate.nvim",
+    lazy = false,
+    keys = {
+      { "<leader>tw", "<cmd>Translate ZH<cr>", desc = "翻译" },
+    },
+    init = function()
+      require("translate").setup({
+        default = {
+          command = "translate_shell",
+        },
+      })
+    end,
   },
 }
